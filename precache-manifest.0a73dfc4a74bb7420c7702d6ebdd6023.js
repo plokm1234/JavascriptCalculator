47b1d1a968eed2c6ab6baf871ccdc3efd183c356
plokm1234 <wyuenab@gmail.com>
2020-01-28 11:35:00 +0800

    Updates

---
+++ precache-manifest.0a73dfc4a74bb7420c7702d6ebdd6023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0f688860aaf19671161ac6212098b4",
+    "url": "/JavascriptCalculator/index.html"
+  },
+  {
+    "revision": "59f8a55fa34f120d9a99",
+    "url": "/JavascriptCalculator/static/css/main.2cbb25ff.chunk.css"
+  },
+  {
+    "revision": "f4131636effcd1bc6a62",
+    "url": "/JavascriptCalculator/static/js/2.ef52ecf4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/JavascriptCalculator/static/js/2.ef52ecf4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59f8a55fa34f120d9a99",
+    "url": "/JavascriptCalculator/static/js/main.e39e7fd7.chunk.js"
+  },
+  {
+    "revision": "ec4364a12ff50c7706da",
+    "url": "/JavascriptCalculator/static/js/runtime-main.63f8e8ad.js"
+  }
+]);
